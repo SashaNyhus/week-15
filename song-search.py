6a7d559ea36691e97b32fb87d214777d95e633aa
SashaVM <sashavictoriam@icloud.com>
2021-06-11 16:29:09 -0700

    added options if song isn't in results can start new search, see another page of results, or cancel the program

--- song-search.py
+++ song-search.py
@@ -2,21 +2,23 @@ import requests
 import json
 
 
-def get_user_song_key():
-    search_query = input("Search for a song")
-    search_results = search_for_song(search_query)
+def get_user_song_key(search_term, results_page):
+    search_results = search_for_song(search_term, results_page)
     results_to_print = get_track_results_to_print(search_results)
     print("We found:")
     print(*results_to_print, sep="\n")
     song_index = int(input(
-        "Which song did you mean? Type the number next to it"
+        "Which song did you mean?\n"
+        "Type the number next to it, or 0 if none of them match"
         ))
-    return search_results["tracks"]["hits"][song_index]["track"]["key"]
+    if(song_index == 0):
+        return 0
+    return search_results["tracks"]["hits"][(song_index - 1)]["track"]["key"]
 
 
-def search_for_song(song):
+def search_for_song(song, page):
     url = "https://shazam.p.rapidapi.com/search"
-    query_str = {"term": song, "locale": "en-US", "offset": "0", "limit": "5"}
+    query_str = {"term": song, "locale": "en-US", "offset": page, "limit": "5"}
     headers = {
         'x-rapidapi-key': "fa28eee3eemsh74c42be02bf9b23p141db5jsnbfb2a373758a",
         'x-rapidapi-host': "shazam.p.rapidapi.com"
@@ -28,7 +30,7 @@ def search_for_song(song):
 def get_track_results_to_print(results_obj):
     track_results = results_obj["tracks"]["hits"]
     array_to_print = []
-    track_index = 0
+    track_index = 1
     for obj in track_results:
         array_to_print.append(
             str(track_index) + ") " +
@@ -39,7 +41,30 @@ def get_track_results_to_print(results_obj):
 
 
 def get_user_song_suggestions():
-    user_song_key = get_user_song_key()
+    choosing_song = True
+    search_query = input("Search for a song")
+    page_offset = 0
+    while(choosing_song):
+        user_song_key = get_user_song_key(search_query, page_offset)
+        if(user_song_key == 0):
+            print(
+                "0) new search\n"
+                "1) show me the next page of results\n"
+                "2) cancel program"
+            )
+            decision = int(input("what would you like to do?"))
+            if(decision == 0):
+                search_query = input("Search for a song")
+                page_offset = 0
+                continue
+            elif(decision == 1):
+                page_offset += 5
+                continue
+            else:
+                return
+        else:
+            choosing_song = False
+            break
     print(user_song_key)
     suggestions_obj = fetch_suggestions(user_song_key)
     if(suggestions_obj):
