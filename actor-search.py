7ea4d8998847f24af66e6f92273859f9670cfec3
SashaVM <sashavictoriam@icloud.com>
2021-06-15 13:23:57 -0700

    display all filmography fixed bug with entries that didn't include "characters" as a key by checking if they did before using it

--- actor-search.py
+++ actor-search.py
@@ -14,23 +14,64 @@ def actorSearch():
         return
     printableKnownFor = []
     for result in knownFor:
+        summary = result["summary"]
+        role = ""
+        if "characters" in summary:
+            role = (summary["category"] + "- " + ", ".join(summary["characters"]))
+        elif "category" in summary:
+            role = summary["category"]
         printableKnownFor.append(
             result["title"]["title"] + 
             " (" + str(result["title"]["year"]) + " " + result["title"]["titleType"] + "): " +
-            (", ".join(result["summary"]["characters"]) or result["summary"]["category"])
+            role
         )
     print(actorName + " is best known for")
     print(*printableKnownFor, sep="\n")
-    # getMore = input("See all filmography? y/n")
-    # if (getMore == "y"):
-    #     filmography = imdb.getAllFilmography(actorKey)
-    #     printableFilmography = []
-    #     for title in filmography["filmography"]:
-    #         printableFilmography.append(
-    #             title["title"] + " (" + title["titleType"] + ", " + (str(title["year"]) or title["status"]) + "): " +
-    #             # (", ".join(title["characters"]) or 
-    #             result["category"]
-    #         )
+    getMore = input("See all filmography? y/n")
+    if (getMore == "y"):
+        filmography = imdb.getAllFilmography(actorKey)
+        printableFilmography = []
+        for title in filmography["filmography"]:
+            titleStatus = ""
+            role = ""
+            if "year" in title:
+                titleStatus = str(title["year"])
+            elif "status" in title:
+                titleStatus = title["status"]
+            if "characters" in title:
+                role = (title["category"] + "- " + ", ".join(title["characters"]))
+            elif "category" in title:
+                role = title["category"]
+            printableFilmography.append(
+                title["title"] + " (" + title["titleType"] + ", " + titleStatus + "): " + role
+            )
+        keepViewing = True
+        indexStart = 0
+        page = 1
+        while(keepViewing):
+            print(actorName + " filmography, page " + str(page))
+            print("\n".join(printableFilmography[indexStart:(indexStart + 24)]))
+            doNext = input(
+                "0) end program \n"
+                "1) see next 25 results \n"
+                "2) see previous 25 results"
+            )
+            if doNext == 1:
+                indexStart += 25
+                page += 1
+                if indexStart > (len(printableFilmography)):
+                    print("end of list reached. returned to top")
+                    indexStart = 0
+                    page = 1
+                continue
+            elif doNext == 2:
+                if indexStart > 0:
+                    indexStart -=25
+                    page -= 1
+                else:
+                    print("already at beginning of list")
+                continue
+            else: break
     return
 
 def getActorFromCharacter():
